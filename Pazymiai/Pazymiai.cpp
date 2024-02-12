cc6989c303b537afe01b183d9100aa02d5d07fdc
AdoJas <147920380+AdoJas@users.noreply.github.com>
2024-02-12 23:27:18 +0200

    pirmine duomenu struktura

--- Pazymiai/Pazymiai.cpp
+++ Pazymiai/Pazymiai.cpp
@@ -1,20 +1,31 @@
-// Pazymiai.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <string>
+#include <iomanip>
+#include <cstring>
+
+using namespace std;
+
+struct studentas {
+    string vardas;
+    string pavarde;
+    float* pazymiai;
+    float pazVid = 0;
+    float egzaminas = 0;
+    float mediana = 0;
+}; 
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int n = 0; //studentu skaicius
+    int x = 0; //pazymiu skaicius
+
+    cout << "Kiek studentu yra grupeje?" << endl;
+    cin >> n;
+    studentas* grupe = new studentas[n];
+
+    cout << "Kiek namu darbu pazymiu turi kiekvienas mokinys?" << endl;
+    cin >> x;
+
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
