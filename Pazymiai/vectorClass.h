68899aa2823a0553070547349e941fac5b2b2bc5
AdoJas <147920380+AdoJas@users.noreply.github.com>
2024-05-13 15:16:10 +0300

    Pridetos pirmosios vectorClass.h funkcijos

--- Pazymiai/vectorClass.h
+++ Pazymiai/vectorClass.h
@@ -5,6 +5,36 @@
 #ifndef OOPUZD_VECTORCLASS_H
 #define OOPUZD_VECTORCLASS_H
 
+template<typename T>
+class Vector{
+private:
+    int size;
+    int capacity;
+    T* elements;
+
+public:
+    Vector();
+    Vector(int n, const T& value);   //+
+    Vector(const Vector &rhs);       //+
+
+    ~Vector();                //+
+
+    int Size();               //+
+    int Capacity();           //+
+    bool isEmpty();           //+
+
+    void PushBack(T &object); //+
+    void PopBack();           //+
+    void Clear();             //+
+    void Resize(int n);       //+
+    void Reserve(int n);      //+
+    void Swap(Vector &rhs);   //+
+    void ShrinkToFit();       //+
+    void Erase(int index);    //+
+    void Insert(int index, T &object);  //+
+    void Assign(int n, const T& value); //+
+
+};
 
 
 #endif //OOPUZD_VECTORCLASS_H
